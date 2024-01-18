7ebe9c0472aec98e4d4611af33da7f4670a262b3
asicoltd <asifhasan099@gmail.com>
2024-01-18 09:10:54 +0600

    add

---
+++ app/src/main/java/com/ewubd/bluechat/matrix_maeny/bluetoothchat/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.ewubd.bluechat.matrix_maeny.bluetoothchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ewubd.bluechat.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
